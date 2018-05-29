b995544ef7c4e8a9abe74f7597a5b8623087c4e0
Unknown <thegreatskywalker@gmail.com>
2018-05-29 17:08:32 +0530

    removed google drive authentication python script from setup
    it causes errors

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -3,8 +3,12 @@ apt-get install aria2
 pip install kaggle
 
 
-wget https://raw.githubusercontent.com/thegreatskywalker/my_deep_learning/master/Scripts/google_drive_authentication.py 	
-chmod +x google_drive_authentication.py
-python google_drive_authentication.py
+
+
+
+
+#wget https://raw.githubusercontent.com/thegreatskywalker/my_deep_learning/master/Scripts/google_drive_authentication.py 	
+#chmod +x google_drive_authentication.py
+#python google_drive_authentication.py
 
 
