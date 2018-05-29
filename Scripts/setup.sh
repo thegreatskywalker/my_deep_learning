41fba3ff7676c07b0e444c5b46887adaf75eb772
Unknown <thegreatskywalker@gmail.com>
2018-05-29 17:01:51 +0530

    added google drive python file link in setup script

--- Scripts/setup.sh
+++ Scripts/setup.sh
@@ -2,7 +2,8 @@ pip install wget
 apt-get install aria2
 pip install kaggle
 
- 	
+
+wget https://raw.githubusercontent.com/thegreatskywalker/my_deep_learning/master/Scripts/google_drive_authentication.py 	
 chmod +x google_drive_authentication.py
 python google_drive_authentication.py
 
