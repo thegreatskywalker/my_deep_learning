7d55e0d2d9b7c3e0ea1a11c68db115ad1ba45948
Unknown <thegreatskywalker@gmail.com>
2018-06-02 14:28:36 +0530

    Revert "added new tensorflow_cpu yml file"
    This reverts commit c62464314255667b4da0115270b7e19eabef60a3.

--- test.py
+++ test.py
@@ -63,7 +63,7 @@ from AutoDownloader import AutoDownloader
 auto_dl = AutoDownloader()
 auto_dl.initiate(project_dir, data_to_download)
 
-auto_dl.showDirectory(project_dir)
+auto_dl.showDirectory(project_dir,True, 2)
 
 user_key = "umnxi6yau47cpp9evkn6nfa5zbpipt"
 token ="aev81xsojcq2ggdpevsia5rzuw5x12"
